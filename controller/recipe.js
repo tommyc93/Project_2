cf2396f1b7fdae140889d13504df99f0b9261f5b
Tommy Chung <tommychung@TC-MacBook-Pro.local>
2021-07-28 15:34:17 -0400

    Update

--- controller/recipe.js
+++ controller/recipe.js
@@ -4,6 +4,7 @@
 const express = require('express')
 const router = express.Router()
 const Recipe = require('../models/recipe.js')
+const Author = require('../models/authors.js')
 
 //Middleware authentication
 const isAuthenticated = (req, res, next) => {
@@ -68,17 +69,19 @@ router.get('/seed', (req, res) => {
             name: "Vegan Pea Soup",
             description: "A vegan soup made coconut creme",
             img: "https://lovingitvegan.com/wp-content/uploads/2019/01/Vegan-Pea-Soup-13.jpg",
+            readyIn: "15 mins",
             serving: 4,
-            ingredients: ["1 Tbsp Olive Oil", "1 Medium Onion", "1 tsp Crushed Garlic", "1 tsp Dried Thyme", "1 tsp Oregano", "6 cups Frozen Peas", "2 cups Vegetable Stock", "14 oz Canned Coconut Cream", "Salt and Pepper to taste"],
+            ingredients: ["1 Tbsp Olive Oil", "1 Medium Onion", "1 Tsp Crushed Garlic", "1 Tsp Dried Thyme", "1 Tsp Oregano", "6 Cups Frozen Peas", "2 Cups Vegetable Stock", "14 Oz Canned Coconut Cream", "Salt and Pepper to taste"],
             directions: ["Add the olive oil to a pot along with the chopped onion, garlic, thyme and oregano and sauté until the onions are softened.", "Add in the frozen peas and toss up with the onions and spices.", "Add in the vegetable stock and coconut cream and bring to the boil. Cook until the peas are firm but tender.", "Blend up the soup ideally using an immersion (handheld) blender or by transferring the soup in stages to the blender jug and blending it and then return it to the pot.", "Add sea salt and ground black pepper to taste.", "Serve with a swirl of coconut cream and a sprinkle of ground black pepper (optional)."]
         },
         {
-            name: "Pea Soup",
-            description: "A vegan soup made with peas",
-            img: "https://lovingitvegan.com/wp-content/uploads/2019/01/Vegan-Pea-Soup-13.jpg",
-            serving: 4,
-            ingredients: ["1 Tbsp Olive Oil", "1 Medium Onion", "1 tsp Crushed Garlic", "1 tsp Dried Thyme", "1 tsp Oregano", "6 cups Frozen Peas", "2 cups Vegetable Stock", "14 oz Canned Coconut Cream", "Salt and Pepper to taste"],
-            directions: ["Add the olive oil to a pot along with the chopped onion, garlic, thyme and oregano and sauté until the onions are softened.", "Add in the frozen peas and toss up with the onions and spices.", "Add in the vegetable stock and coconut cream and bring to the boil. Cook until the peas are firm but tender.", "Blend up the soup ideally using an immersion (handheld) blender or by transferring the soup in stages to the blender jug and blending it and then return it to the pot.", "Add sea salt and ground black pepper to taste.", "Serve with a swirl of coconut cream and a sprinkle of ground black pepper (optional)."]
+            name: "Les Halles Vichyssoise",
+            description: "A cold soup recipe made by Anthony Bourdain",
+            img: "https://img.sndimg.com/food/image/upload/c_thumb,q_80,w_721,h_406/v1/img/recipes/13/60/57/jP0sfxNjTbmrgou3n6pb_image.jpg",
+            readyIn: "1 hr 20 mins",
+            serving: 6,
+            ingredients: ["4 Tbsp Butter", "8 Leeks, thinly sliced", "2 Medium Potatoes cubed", "2 Cups Chicken Stock", "2 Cups Heavy Cream", "4 Chives, finely chopped", "1 Pinch Nutmeg", "Salt and Pepper to taste"],
+            directions: ["In a large, heavy bottom pot, melt butter over medium-low heat. Once butter is melted, add the leeks and sweat for 5 minutes, making sure they do not take on any color.", "Add potatoes and cook for a minute or two, stirring a few times.", "Stir in the chicken broth and bring to a boil.", "Reduce heat to a simmer. Cook on low heat, gently simmering for 35 minutes, or until the leeks and potatoes are very soft. Allow to cool for a few minutes.", "Slowly, and in SMALL batches, puree the soup at a high speed in the blender. Do this bit by bit, never filling the blender too high. Make sure the benders lid is on, and lean on the top when you turn on. If not the burn you will get is awful, and a most frequent accident in even professional kitchens.", "Return soup to the cooking pot and whisk in cream and nutmeg. Season with salt and pepper. Return to a boil, reduce to simmer and cook 5 minutes. If you want to thin soup out, add more broth, if needed.", "Transfer soup to the mixing bowl an chill over the ice bath, stirring occasionally. When soup is at Room temperature, and only at room temperature, cover in plastic wrap and put into the refrigerator to cool.", "Check seasoning, sprinkle with chives and serve in chilled bowls."]
         },
     ],
     (error, data) => {
